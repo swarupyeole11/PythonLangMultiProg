fef59c2b8413408ab2952415c83c098fb41ab94f
swarupyeole <swarupyeole11@gmail.com>
2023-10-09 20:16:34 +0530

    completed the function to get news of that specific country

--- ex9_apicalls.py
+++ ex9_apicalls.py
@@ -9,18 +9,37 @@ load_dotenv()
 NEWS_API_KEY = os.getenv("NEWS_API_KEY")
 
 def CountryNews():
-    return 0
+    
+    country_name = input("Enter the name of the country you want new from type : \n us : United States \n in : India  \n")
+    url = (f'https://newsapi.org/v2/top-headlines?country={country_name}&from=2023-10-08&to=2023-10-08&sortBy=popularity&apiKey={NEWS_API_KEY}')
+    response = requests.get(url)
+    data = response.json()
+    
+    try : 
+          useful_data = []
+          counter = 0
+          for datapoint in data['articles'] :
+           counter+=1
 
-def NewspaperNews():
-    return 0
+           if counter <= 10 :
+            useful_data.append(datapoint['url'])
+            #stores the url for top 10 news in the file News.txt
+            with open("News.txt",'a') as file :
+              file.write(datapoint['url']+"\n")
 
-def GenreNews():
-    return 0
+           else :
+             break
+    except :
+         print(data)
+
+        
+    
+    print(useful_data)
 
 #  function to get the top 10 news from the news api
 def AllTop10News():
     
-    url = (f'https://newsapi.org/v2/everything?q=a&from=2023-10-08&to=2023-10-08&sortBy=popularity&apiKey={NEWS_API_KEY}')
+    url = (f'https://newsapi.org/v2/everything?country=us&from=2023-10-08&to=2023-10-08&sortBy=popularity&apiKey={NEWS_API_KEY}')
     response = requests.get(url)
     data = response.json()
     useful_data = []
@@ -32,7 +51,7 @@ def AllTop10News():
          if counter <= 10 :
           useful_data.append(datapoint['url'])
           #stores the url for top 10 news in the file News.txt
-          with open("News.txt",'+a') as file :
+          with open("News.txt",'w') as file :
               file.write(datapoint['url']+"\n")
 
          else :
@@ -49,7 +68,8 @@ def News():
     if user_input == 1:
         AllTop10News()
     else :
-        print("e")
+        CountryNews()
+
 
 News()
 
